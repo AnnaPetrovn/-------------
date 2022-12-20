2014a17f5edc92a50dfcfb0fc6f80e88076f2508
Anna <annapn5@mail.ru>
2022-12-20 19:24:06 +0600

    программа 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿void GetLastDigit(int number)
+{
+    if (number < 1000 && number > 99)
+
+    {
+        int lastDigit = number % 10;
+        Console.WriteLine(lastDigit);
+    }
+}
+
+int tu = 567;
+GetLastDigit (tu);
+
+
+
+
+
